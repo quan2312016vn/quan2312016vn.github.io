331573008a686b1805ecbb95b9dd45f18fd2c4ad
quan2312016vn <quan2312016vn@gmail.com>
2017-08-11 22:54:07 +0700

    Update index.js

--- login/index.js
+++ login/index.js
@@ -3,7 +3,7 @@ var user=document.getElementById("user").value;
 var pass=document.getElementById("pass").value;
 if (user==pass && user!=""){
 
-  document.getElementById('link').href="pornhub.com"
+  document.getElementById('link').href="https://pornhub.com/"
 }else{
     document.getElementById('check').style.display="inline";
 }
