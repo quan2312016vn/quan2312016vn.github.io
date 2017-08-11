979ada77ebcbac24e959357584766a53212d283b
quan2312016vn <quan2312016vn@gmail.com>
2017-08-11 22:13:12 +0700

    Add files via upload

---
+++ login/index.js
@@ -0,0 +1,10 @@
+function href(){
+var user=document.getElementById("user").value;
+var pass=document.getElementById("pass").value;
+if (user==pass && user!=""){
+
+  document.getElementById('link').href="pornhub.com"
+}else{
+    document.getElementById('check').style.display="inline";
+}
+}
